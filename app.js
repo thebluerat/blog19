4579b73f8dd6d17b19555260381e0207179e3600
thebluerat <thebluerat@naver.com>
2019-10-21 20:30:20 +0900

    메인 화면 만들고 저널 페이지 만드는 중. 달력 붙여야 함

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('asdf');
+console.time('Server ON Time');
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.use('/static', express.static('static'));
+
+app.get('/', function(req, res) {
+    fs.readFile(__dirname + '/view/index.html', 'utf8', function(err, data) {
+        if(err) {
+            res.statusCode = 404;
+            res.send('수민이가 알려준다 여기 주소없음 ㅋㅋ!');
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+app.get('/journal', function(req, res) {
+    fs.readFile(__dirname + '/view/journal.html', 'utf8', function(err, data) {
+        if(err) {
+            res.statusCode = 404;
+            res.send('수민이가 알려준다 여기 주소없음 ㅋㅋ!');
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+app.listen(3000, '127.0.0.1', function() {
+    console.timeEnd('Server ON Time');
+});
---
+++ node.js
@@ -0,0 +1,5 @@
+console.log('nodeman');
+
+exports.daehoon = function() {
+    console.log('대훈이는 잘생겼다.')
+}
